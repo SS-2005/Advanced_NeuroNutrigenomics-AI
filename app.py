4cbb2c6168c8a784c99ee6452b7fb7b260d36f20
Sahil Shaikh <sahil.shaikh24@aiml.sce.edu.in>
2025-09-20 12:33:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -925,5 +925,5 @@ def generate_treatment_plan_dict(depression_pred, bipolar_pred, anxiety_pred):
 
     return treatment_plan
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
